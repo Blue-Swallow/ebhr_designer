d169d3adcf826d99e66a1c227fa0b1750b2eb8cf
Blue-Swarrow <ayumu.colossians2.6@gmail.com>
2020-09-27 19:03:05 +0900

    add: enable to calculate the case of N2 mixture

--- main_desgin_liquid.py
+++ main_desgin_liquid.py
@@ -14,6 +14,7 @@ from datetime import datetime
 from tqdm import tqdm
 from scipy import optimize
 from cea_post import Read_datset
+from cea_exe import CEA_onetime_execute as cexe
 
 # %%
 class Cal_excond:
@@ -46,7 +47,7 @@ class Cal_excond:
         "m": float, optional, exponent of oxidizer port velocity
         "alpha": float, optional, SI-unit experimental constant
     """
-    def __init__(self, cond, const, **kwargs):
+    def __init__(self, cond, liquid, cea, const, **kwargs):
         """constructer
 
         Parameter
@@ -77,6 +78,8 @@ class Cal_excond:
             "alpha": float, optional, SI-unit experimental constant
         """
         self.cond = cond
+        self.liquid = liquid
+        self.cea = cea
         self.model_const = const
         self.d = self.cond["d"]
         self.N = self.cond["N"]
@@ -86,12 +89,15 @@ class Cal_excond:
         self.Rm = cond["Rm"]
         self.mu = cond["mu"]
         self.Tox = cond["Tox"]
-        self.cea_path = cond["cea_path"]
-        self.C = cond["C"]
-        self.be = cond["be"]
-        self.d_o = cond["d_o"]
-        self.rho_ox = cond["rho_ox"]
+        self.C = liquid["C"]
+        self.be = liquid["be"]
+        self.d_o = liquid["d_o"]
+        self.rho_ox = liquid["rho_ox"]
         self.a = 1 - self.N*np.power(self.d, 2)/np.power(self.Df, 2)
+        if cea["mode_n2"]:
+            self.cea_path = None
+        else:
+            self.cea_path = cea["cea_path"]
         if self.model_const["mode"] == "C1C2":
             self.C1 = self.model_const["C1"]
             self.C2 = self.model_const["C2"]
@@ -142,7 +148,7 @@ class Cal_excond:
             # Vf = func_Vf_prop(Vox, Pc, self.alpha, n=self.n)
             pass
         of = func_of(Vox, Vf, self.rho_f, self.a, Pc, self.Rm, self.Tox)
-        func_cstr = gen_func_cstr(self.cea_path)
+        func_cstr = gen_func_cstr(self.cea)
         Pc_cal = func_Pc_cal(of, Pc, mox, func_cstr, Dt, self.eta)
         diff = Pc_cal - Pc
         error = diff/Pc
@@ -175,7 +181,7 @@ class Cal_excond:
             "ustr_lam": float, [m/s] friction velocity when the flow is laminar
             "ustr_turb": float, [m/s] friction velocity when the flow is turbulence
         """
-        func_cstr = gen_func_cstr(self.cea_path)
+        func_cstr = gen_func_cstr(self.cea)
         try:
             Pc = optimize.newton(self._iterat_func_Pc_, Pc_init, maxiter=10, tol=1.0e-3, args=(mox, Dt))
         except (RuntimeError, RuntimeWarning):
@@ -228,11 +234,10 @@ class Cal_excond:
         error: float
             relative error of cmaber pressure between assumed and calculated value
         """
-        # mox = func_mox_liquid(Pup, Pc, self.C, self.be, self.d_o, self.rho_ox)
         Vox = func_Vox_liquid(mox, self.rho_ox, self.Df, self.a)
         Vf = func_Vf_liquid(Vox, Pc, self.d, self.C11, self.C12, self.C21, self.C22, self.m1, self.m2, n=self.n)
         of = func_of_liquid(mox, Vf, self.rho_f, self.a, self.Df)
-        func_cstr = gen_func_cstr(self.cea_path)
+        func_cstr = gen_func_cstr(self.cea)
         Pc_cal = func_Pc_cal(of, Pc, mox, func_cstr, Dt, self.eta)
         diff = Pc_cal - Pc
         error = diff/Pc
@@ -266,7 +271,7 @@ class Cal_excond:
             "ustr_lam": float, [m/s] friction velocity when the flow is laminar
             "ustr_turb": float, [m/s] friction velocity when the flow is turbulence
         """
-        func_cstr = gen_func_cstr(self.cea_path)
+        func_cstr = gen_func_cstr(self.cea)
         warnings.filterwarnings("error")
         try:
             Pc = optimize.newton(self._iterat_func_Pc_liquid_, Pc_init, maxiter=50, tol=1.0e-3, args=(mox, Dt))
@@ -280,7 +285,7 @@ class Cal_excond:
         Vox = func_Vox_liquid(mox, self.rho_ox, self.Df, self.a)
         Vf = func_Vf_liquid(Vox, Pc, self.d, self.C11, self.C12, self.C21, self.C22, self.m1, self.m2, n=self.n)
         of = func_of_liquid(mox, Vf, self.rho_f, self.a, self.Df)
-        func_cstr = gen_func_cstr(self.cea_path)
+        func_cstr = gen_func_cstr(self.cea)
         cstr = self.eta* func_cstr(of, Pc)
         Re = self.rho_ox*Vox*self.d/self.mu
         Pup = func_Pup_liquid(mox, Pc, self.C, self.be, self.d_o, self.rho_ox)
@@ -300,9 +305,9 @@ class Cal_excond:
 
 
 class Gen_excond_table(Cal_excond):
-    def __init__(self, cond, const, row_range=None, Dt_range=None, **kwargs):
-        super().__init__(cond, const)
-        self.row_range = row_range
+    def __init__(self, cond, liquid, cea, const, mox_range=None, Dt_range=None, **kwargs):
+        super().__init__(cond, liquid, cea, const)
+        self.mox_range = mox_range
         self.Dt_range = Dt_range
 
     def _input_range_(self):
@@ -310,27 +315,21 @@ class Gen_excond_table(Cal_excond):
         
         Returns
         -------
-        row_range: 1d-ndarray of float
+        mox_range: 1d-ndarray of float
             [kg/s] or [Pa] the range of oxidizer mass flow rate or reservoir pressure
         Dt_range: 1d-ndarray of float
             [m] the range of nozzle throat diameter
         """
-        # if self.cond["mode_liquid"]:
-        #     print("\n\nInput the range of Pup [MPa], orifice up pressure, where you want to generate the table." )
-        #     print("\ne.g. If the range is 1.0 to 5.0 MPa and the interval is 0.1 MPa\n1.0 5.0 0.1")
-        #     tmp = list(map(lambda x: float(x) ,input().split()))
-        #     row_range = np.arange(tmp[0], tmp[1]+tmp[2]/2, tmp[2])*1.0e+6 # generate range and convert [MPa] to [Pa]
-        # else:
         print("\n\nInput the range of mox [g/s], oxidizer mass flow rate, where you want to generate the table." )
         print("\ne.g. If the range is 10.0 to 20.0 g/s and the interval is 1.0 g/s\n10.0 20.0 1.0")
         tmp = list(map(lambda x: float(x) ,input().split()))
-        row_range = np.arange(tmp[0], tmp[1]+tmp[2]/2, tmp[2])*1.0e-3 # generate range and convert [g/s] to [kg/s]
+        mox_range = np.arange(tmp[0], tmp[1]+tmp[2]/2, tmp[2])*1.0e-3 # generate range and convert [g/s] to [kg/s]
 
         print("\n\nInput the range of Dt [mm], nozzle throat diameter, where you want to generate the table." )
         print("\ne.g. If the range is 5.0 to 10.0 mm and the interval is 1.0 mm\n5.0 10.0 1.0")
         tmp = list(map(lambda x: float(x) ,input().split()))
         Dt_range = np.arange(tmp[0], tmp[1]+tmp[2]/2, tmp[2])*1.0e-3 # generate range and convert [mm] to [m]
-        return row_range, Dt_range
+        return mox_range, Dt_range
 
     def gen_table(self):
         """ function to generate combustion parameter
@@ -350,9 +349,9 @@ class Gen_excond_table(Cal_excond):
             "ustr_lam": pandas.DataFrame, [m/s] friction velocity when the flow is laminar
             "ustr_turb": pandas.DataFrame, [m/s] friction velocity when the flow is turbulence
         """
-        if self.row_range is None or self.Dt_range is None:
-            self.row_range , self.Dt_range = self._input_range_()
-        df_base = pd.DataFrame({}, index=self.row_range)
+        if self.mox_range is None or self.Dt_range is None:
+            self.mox_range , self.Dt_range = self._input_range_()
+        df_base = pd.DataFrame({}, index=self.mox_range)
         df_pc = df_base.copy(deep=True)
         df_vox = df_base.copy(deep=True)
         df_vf = df_base.copy(deep=True)
@@ -372,18 +371,18 @@ class Gen_excond_table(Cal_excond):
             ustr_lam = np.array([])
             ustr_turb = np.array([])
             Pup = np.array([])
-            for row_val in tqdm(self.row_range, desc="mox or Pup loop", leave=False):
-                if self.cond["mode_liquid"]:
-                    tmp = self.get_excond_liquid(row_val, Dt, Pc_init=0.2e+6)
+            for mox_val in tqdm(self.mox_range, desc="mox or Pup loop", leave=False):
+                if self.liquid["mode_liquid"]:
+                    tmp = self.get_excond_liquid(mox_val, Dt, Pc_init=0.2e+6)
                 else:
-                    tmp = self.get_excond(row_val, Dt, Pc_init=0.1e+6)
+                    tmp = self.get_excond(mox_val, Dt, Pc_init=0.1e+6)
                 Pc = np.append(Pc, tmp["Pc"])
                 Vox = np.append(Vox, tmp["Vox"])
                 Vf = np.append(Vf, tmp["Vf"])
                 of = np.append(of, tmp["of"])
                 cstr_ex = np.append(cstr_ex, tmp["cstr_ex"])
                 Re = np.append(Re, tmp["Re"])
-                if self.cond["mode_liquid"]:
+                if self.liquid["mode_liquid"]:
                     ustr_lam = np.append(ustr_lam, np.nan)
                     ustr_turb = np.append(ustr_turb, np.nan)
                     Pup = np.append(Pup, tmp["Pup"])
@@ -423,7 +422,9 @@ class Gen_excond_table(Cal_excond):
         if fldname is None:
             fldname = datetime.now().strftime("%Y_%m%d_%H%M%S")   # folder name which contain animation and figure of calculation result
         os.mkdir(fldname)
-        dic_json = {"CALCOND": self.cond,
+        dic_json = {"PARAM_EX": self.cond,
+                    "PARAM_LIQUID": self.liquid,
+                    "PARAM_CEA": self.cea,
                     "MODELCONST": self.model_const
                     }
         with open(os.path.join(fldname, "cond.json"), "w") as f:
@@ -487,11 +488,26 @@ def func_ustr_turb(P, u, T, Rm, d, mu):
     ustr = np.sqrt(tau/rho)
     return ustr
 
-def gen_func_cstr(cea_path):
-    func = Read_datset(cea_path).gen_func("CSTAR")
+def gen_func_cstr(param_cea):
+    if param_cea["mode_n2"]: # generate cstr function which use one-time CEA execution
+        obj = cexe()
+        def func(of, Pc):
+            wt_n2 = param_cea["massfrac_n2"]*100
+            wt_oxid = np.power((1+1/of), -1) *(100-wt_n2)
+            wt_fuel = np.power((1+of), -1) *(100-wt_n2)
+            list_species = [
+                {"name": param_cea["name_oxid"], "wt": wt_oxid, "temp": param_cea["temp_oxid"], "h": "", "elem": ""},
+                {"name": param_cea["name_fuel"], "wt": wt_fuel, "temp": param_cea["temp_fuel"], "h": param_cea["enthalpy"], "elem": param_cea["element"]},
+                {"name": "N2", "wt": wt_n2, "temp": 280, "h": "", "elem": ""}
+                ]
+            res = obj.onetime_exe_name(param_cea["eq_option"], list_species, Pc, param_cea["eps"])
+            cstr = res["rock"]["CSTAR"][0]
+            return cstr
+    else: # generate cstr function which use .csv data base
+        func = Read_datset(param_cea["cea_path"]).gen_func("CSTAR")
     return(func)
 
-def func_Pc_cal(of, Pc, mox, func_cstr, Dt, eta):
+def func_Pc_cal(of, Pc, mox, func_cstr, Dt, eta, wt_n2):
     cstr = func_cstr(of,Pc)
     At = np.pi*np.power(Dt, 2)/4
     Pc_cal = eta*cstr*mox*(1 + 1/of)/At
@@ -500,22 +516,40 @@ def func_Pc_cal(of, Pc, mox, func_cstr, Dt, eta):
 # %%
 if __name__ == "__main__":
 # %%
-    CALCOND = {"d": 0.5e-3,      # [m] port diameter
-               "N": 132,        # [-] the number of port
-               "Df": 40.7e-3,     # [m] fuel outer diameter
-               "eta": 1.0,      # [-] efficiency of characteristic exhaust velocity
-               "rho_f": 1191,   # [kg/m3] density of fuel
-               "Rm": 259.8,     # [J/kg/K] gas constant
-               "Tox": 300,      # [K] oxidizer tempereture
-               "mu": 20.3e-6,  # [Pa-s] oxidizer dynamic viscosity
-               "mode_liquid": True, # mode selection; using liquid oxidizer or using gasous oxidizer.
-               "C": 0.61,       # discharge coefficient of orifice
-               "be": 0.25,      # diamter ratio between pipe and orifice diameter
-               "d_o": 1.7e-3,   # [m] diameter of orifice
-               "rho_ox": 1190,  # [kg/m3] density of liquid oxidizer
-            #    "cea_path": os.path.join("cea_db", "GOX_CurableResin", "csv_database")   # relative folder path to CEA .csv data-base
-               "cea_path": os.path.join("cea_db", "LOX_CurableResin", "csv_database")   # relative folder path to CEA .csv data-base
-               }
+    PARAM_EX = {"d": 0.5e-3,     # [m] port diameter
+                "N": 132,        # [-] the number of port
+                "Df": 40.7e-3,   # [m] fuel outer diameter
+                "eta": 1.0,      # [-] efficiency of characteristic exhaust velocity
+                "rho_f": 1191,   # [kg/m3] density of fuel
+                "Rm": 259.8,     # [J/kg/K] gas constant
+                "Tox": 300,      # [K] oxidizer tempereture
+                "mu": 20.3e-6    # [Pa-s] oxidizer dynamic viscosity
+                }
+
+    PARAM_LIQUID = {"mode_liquid": True, # mode selection; using liquid oxidizer or using gasous oxidizer.
+                    "C": 0.61,       # discharge coefficient of orifice
+                    "be": 0.25,      # diamter ratio between pipe and orifice diameter
+                    "d_o": 1.7e-3,   # [m] diameter of orifice
+                    "rho_ox": 1190   # [kg/m3] density of liquid oxidizer
+                    }
+
+    PARAM_CEA = {"cea_path": os.path.join("cea_db", "LOX_CurableResin", "csv_database"),   # relative folder path to CEA .csv data-base
+                #  "cea_path": os.path.join("cea_db", "GOX_CurableResin", "csv_database"),   # relative folder path to CEA .csv data-base
+                 "mode_n2": True,               # mode selection; using Gasous N2 for pressurization or not.
+                                                # If True, this code execute CEA as a single execute mode using following data,
+                                                # If False, this code execute CEA using the .csv data base assigned at the above key: "cea_db".
+                 "massfrac_n2": 0.1,            # mass fraction of Gaseous N2 to fuel and oxidizer mass flow rate
+                 "eq_option": "frozen nfz=2",   # (CEA input) equilibrium option for NASA-CEA
+                 "eps": 1.0,                    # (CEA input) nozzle opening ratio
+                 "name_oxid": "O2(L)",          # (CEA input) the name of oxidizer
+                 "temp_oxid": 90,               # (CEA input) [K] the temperature of oxidizer
+                 "name_fuel": "CurableResin",   # (CEA input) the name of fuel
+                 "temp_fuel": 90,               # (CEA input) [K] the temperature of fuel
+                 "enthalpy": -296.9636,         # (CEA input) [kJ/mol]
+                 "element": "C 16.0873 H 20.6143 O 3.96810"     # (CEA input) the kind of element and its moleculer number
+                }
+              
+
     CONST_VF = {"mode": "LIQUID",     # mode selectioin.
                                     # "C1C2" mode uses the following experimental formula: Vf = (C1/Vox+C2)P^n, 
                                     # "EXP" mode uses the following experimental formula: Vf = beta*P^n*Vox^m,
@@ -530,12 +564,12 @@ if __name__ == "__main__":
                 "beta": None,       # SI-unit, optional. experimental constant
                 "m": None,          # [-] optional. exponent of oxidizer port velocity
                 "alpha": None,      # SI-unit, optional. experimental constant
-                "C11": -104e-9,
-                "C12": 3.55e-9,
-                "m1": 0.715,
-                "C21": 43.4e-9,
-                "C22": -0.905e-9,
-                "m2": 0.426
+                "C11": -104e-9,     # SI-unit, optional. experimental constant
+                "C12": 3.55e-9,     # SI-unit, optional. experimental constant
+                "m1": 0.715,        # [-] optional. exponent of port diameter
+                "C21": 43.4e-9,     # SI-unit, optional. experimental constant
+                "C22": -0.905e-9,   # SI-unit, optional. experimental constant
+                "m2": 0.426         # [-] optional. exponent of port diameter
                 }
 
 # %%
@@ -545,7 +579,7 @@ if __name__ == "__main__":
 
     MOX_RANGE = np.arange(5.0, 50.0, 1.0)*1e-3
     DT_RANGE = np.arange(4.0, 6.0, 0.5)*1e-3
-    inst = Gen_excond_table(CALCOND, CONST_VF, row_range=MOX_RANGE, Dt_range=DT_RANGE)
+    inst = Gen_excond_table(PARAM_EX, PARAM_LIQUID, PARAM_CEA, CONST_VF, mox_range=MOX_RANGE, Dt_range=DT_RANGE)
 
 # %%
     # inst = Gen_excond_table(CALCOND, CONST_VF)
