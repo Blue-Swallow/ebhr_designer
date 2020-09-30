4c43286b2c35964b0dd148fa9eed0c5ca21ac920
Blue-Swallow <ayumu.colossians2.6@gmail.com>
2020-09-30 23:17:11 +0900

    change: read condtion from cond.py

--- main_desgin_liquid.py
+++ main_desgin_liquid.py
@@ -9,6 +9,7 @@ import numpy as np
 import pandas as pd
 import os
 import json
+import sys
 import warnings
 from datetime import datetime
 from tqdm import tqdm
@@ -429,7 +430,7 @@ class Gen_excond_table(Cal_excond):
         """
         if fldname is None:
             fldname = datetime.now().strftime("%Y_%m%d_%H%M%S")   # folder name which contain animation and figure of calculation result
-        os.mkdir(fldname)
+            os.mkdir(fldname)
         dic_json = {"PARAM_EX": self.cond,
                     "PARAM_LIQUID": self.liquid,
                     "PARAM_CEA": self.cea,
@@ -520,81 +521,118 @@ def func_Pc_cal(of, Pc, mox, func_cstr, Dt, eta, wt_n2):
     At = np.pi*np.power(Dt, 2)/4
     Pc_cal = eta*cstr*mox*(1 + 1/of + (1+1/of)*wt_n2/(1.0-wt_n2))/At
     return(Pc_cal)
-   
+
+
+
+def read_cond():
+    """ Assign the name of folder which contains calculation condition and will contain calculation result.
+    
+    Returns
+    -------
+    fldname: string
+        the name of folder
+    dic_cond: dict of dict
+        dictionary of the dictioinary which contains calculating condition
+        keylist: "PARAM_EXCOND", "PARAM_CALCOND", "PARAM_MODELCONST", "CEA_FLDPATH", "CEA_FLDPATH", "CEA_FLDPATH", "FUNCLIST_CEA", "PARAM_PLOT"
+    func_mox: function
+        function for oxidizer mass flow rate. this is the function of time.
+    """
+    name_cond_py = "cond.py" 
+    fldname = input("Please input the name of folder which contains calculating condition such as {}\n>>".format(name_cond_py))
+    if os.path.exists(fldname):
+        if os.path.exists(os.path.join(fldname, name_cond_py)):
+            sys.path.append(os.path.join(os.path.dirname(__file__), fldname))
+            import cond
+            dic_cond = {"PARAM_EX": cond.PARAM_EX,
+                        "PARAM_LIQUID": cond.PARAM_LIQUID,
+                        "PARAM_CEA": cond.PARAM_CEA,
+                        "CONST_VF": cond.CONST_VF
+                        }
+        else:
+            print("There is no python module of calculation condition, \"{}\"".format(name_cond_py))
+            sys.exit()
+    else:
+        print("There is no such a folder, \"{}\"".format(fldname))
+        sys.exit()
+    return fldname, dic_cond
+
+
 # %%
 if __name__ == "__main__":
 # %%
-    PARAM_EX = {"d": 0.5e-3,     # [m] port diameter
-                "N": 132,        # [-] the number of port
-                "Df": 40.7e-3,   # [m] fuel outer diameter
-                "eta": 1.0,      # [-] efficiency of characteristic exhaust velocity
-                "rho_f": 1191,   # [kg/m3] density of fuel
-                "Rm": 259.8,     # [J/kg/K] gas constant
-                "Tox": 300,      # [K] oxidizer tempereture
-                "mu": 20.3e-6    # [Pa-s] oxidizer dynamic viscosity
-                }
+## following part is for the debugging mode
+    # PARAM_EX = {"d": 0.5e-3,     # [m] port diameter
+    #             "N": 132,        # [-] the number of port
+    #             "Df": 40.7e-3,   # [m] fuel outer diameter
+    #             "eta": 1.0,      # [-] efficiency of characteristic exhaust velocity
+    #             "rho_f": 1191,   # [kg/m3] density of fuel
+    #             "Rm": 259.8,     # [J/kg/K] gas constant
+    #             "Tox": 300,      # [K] oxidizer tempereture
+    #             "mu": 20.3e-6    # [Pa-s] oxidizer dynamic viscosity
+    #             }
 
-    PARAM_LIQUID = {"mode_liquid": True, # mode selection; using liquid oxidizer or using gasous oxidizer.
-                    "C": 0.61,       # discharge coefficient of orifice
-                    "be": 0.25,      # diamter ratio between pipe and orifice diameter
-                    "d_o": 1.7e-3,   # [m] diameter of orifice
-                    "rho_ox": 1190   # [kg/m3] density of liquid oxidizer
-                    }
+    # PARAM_LIQUID = {"mode_liquid": True, # mode selection; using liquid oxidizer or using gasous oxidizer.
+    #                 "C": 0.61,       # discharge coefficient of orifice
+    #                 "be": 0.25,      # diamter ratio between pipe and orifice diameter
+    #                 "d_o": 1.7e-3,   # [m] diameter of orifice
+    #                 "rho_ox": 1190   # [kg/m3] density of liquid oxidizer
+    #                 }
 
-    PARAM_CEA = {"cea_path": os.path.join("cea_db", "GOX_CurableResin", "csv_database"),   # relative folder path to CEA .csv data-base
-                #  "cea_path": os.path.join("cea_db", "LOX_CurableResin", "csv_database"),   # relative folder path to CEA .csv data-base
-                 "mode_n2": False,               # mode selection; using Gasous N2 for pressurization or not.
-                                                # If True, this code execute CEA as a single execute mode using following data,
-                                                # If False, this code execute CEA using the .csv data base assigned at the above key: "cea_db".
-                 "massfrac_n2": 0.1,            # mass fraction of Gaseous N2 to fuel and oxidizer mass flow rate
-                 "eq_option": "frozen nfz=2",   # (CEA input) equilibrium option for NASA-CEA
-                 "eps": 1.0,                    # (CEA input) nozzle opening ratio
-                 "name_oxid": "O2(L)",          # (CEA input) the name of oxidizer
-                 "temp_oxid": 90,               # (CEA input) [K] the temperature of oxidizer
-                 "name_fuel": "CurableResin",   # (CEA input) the name of fuel
-                 "temp_fuel": 90,               # (CEA input) [K] the temperature of fuel
-                 "enthalpy": -296.9636,         # (CEA input) [kJ/mol]
-                 "element": "C 16.0873 H 20.6143 O 3.96810"     # (CEA input) the kind of element and its moleculer number
-                }
+    # PARAM_CEA = {"cea_path": os.path.join("cea_db", "GOX_CurableResin", "csv_database"),   # relative folder path to CEA .csv data-base
+    #             #  "cea_path": os.path.join("cea_db", "LOX_CurableResin", "csv_database"),   # relative folder path to CEA .csv data-base
+    #              "mode_n2": False,               # mode selection; using Gasous N2 for pressurization or not.
+    #                                             # If True, this code execute CEA as a single execute mode using following data,
+    #                                             # If False, this code execute CEA using the .csv data base assigned at the above key: "cea_db".
+    #              "massfrac_n2": 0.1,            # mass fraction of Gaseous N2 to fuel and oxidizer mass flow rate
+    #              "eq_option": "frozen nfz=2",   # (CEA input) equilibrium option for NASA-CEA
+    #              "eps": 1.0,                    # (CEA input) nozzle opening ratio
+    #              "name_oxid": "O2(L)",          # (CEA input) the name of oxidizer
+    #              "temp_oxid": 90,               # (CEA input) [K] the temperature of oxidizer
+    #              "name_fuel": "CurableResin",   # (CEA input) the name of fuel
+    #              "temp_fuel": 90,               # (CEA input) [K] the temperature of fuel
+    #              "enthalpy": -296.9636,         # (CEA input) [kJ/mol]
+    #              "element": "C 16.0873 H 20.6143 O 3.96810"     # (CEA input) the kind of element and its moleculer number
+    #             }
               
 
-    CONST_VF = {"mode": "LIQUID",     # mode selectioin.
-                                    # "C1C2" mode uses the following experimental formula: Vf = (C1/Vox+C2)P^n, 
-                                    # "EXP" mode uses the following experimental formula: Vf = beta*P^n*Vox^m,
-                                    # "PROP" mode uses the following experimental formula: Vf = alpha*P^n
-                                    # "LIQUID" mode for liquid oxidizer uses the following experimental fromaula:
-                                    #  Vf = (C1(d)/Vox+C2(d))P^n, C1(d) = (C11*d^m1 + C12), C2(d) = (C21*d^m2 + C22)
-                "n": 1.0,           # pressure exponent
-                "C1": 9.34e-8,      # SI-unit, optional. experimental constant
-                # "C1": 13.9e-8,      # SI-unit, optional. experimental constant
-                "C2": 2.46e-9,      # SI-unit, optional. experimental constant
-                # "C2": 1.61e-9,      # SI-unit, optional. experimental constant
-                "beta": None,       # SI-unit, optional. experimental constant
-                "m": None,          # [-] optional. exponent of oxidizer port velocity
-                "alpha": None,      # SI-unit, optional. experimental constant
-                "C11": -104e-9,     # SI-unit, optional. experimental constant
-                "C12": 3.55e-9,     # SI-unit, optional. experimental constant
-                "m1": 0.715,        # [-] optional. exponent of port diameter
-                "C21": 43.4e-9,     # SI-unit, optional. experimental constant
-                "C22": -0.905e-9,   # SI-unit, optional. experimental constant
-                "m2": 0.426         # [-] optional. exponent of port diameter
-                }
+    # CONST_VF = {"mode": "LIQUID",     # mode selectioin.
+    #                                 # "C1C2" mode uses the following experimental formula: Vf = (C1/Vox+C2)P^n, 
+    #                                 # "EXP" mode uses the following experimental formula: Vf = beta*P^n*Vox^m,
+    #                                 # "PROP" mode uses the following experimental formula: Vf = alpha*P^n
+    #                                 # "LIQUID" mode for liquid oxidizer uses the following experimental fromaula:
+    #                                 #  Vf = (C1(d)/Vox+C2(d))P^n, C1(d) = (C11*d^m1 + C12), C2(d) = (C21*d^m2 + C22)
+    #             "n": 1.0,           # pressure exponent
+    #             "C1": 9.34e-8,      # SI-unit, optional. experimental constant
+    #             # "C1": 13.9e-8,      # SI-unit, optional. experimental constant
+    #             "C2": 2.46e-9,      # SI-unit, optional. experimental constant
+    #             # "C2": 1.61e-9,      # SI-unit, optional. experimental constant
+    #             "beta": None,       # SI-unit, optional. experimental constant
+    #             "m": None,          # [-] optional. exponent of oxidizer port velocity
+    #             "alpha": None,      # SI-unit, optional. experimental constant
+    #             "C11": -104e-9,     # SI-unit, optional. experimental constant
+    #             "C12": 3.55e-9,     # SI-unit, optional. experimental constant
+    #             "m1": 0.715,        # [-] optional. exponent of port diameter
+    #             "C21": 43.4e-9,     # SI-unit, optional. experimental constant
+    #             "C22": -0.905e-9,   # SI-unit, optional. experimental constant
+    #             "m2": 0.426         # [-] optional. exponent of port diameter
+    #             }
+## The above section is for debugging mode
+# %%
+## Read the condition file from assigned folder
+    FLDNAME, DIC_COND = read_cond()
+    PARAM_EX = DIC_COND["PARAM_EX"]
+    PARAM_LIQUID = DIC_COND["PARAM_LIQUID"]
+    PARAM_CEA = DIC_COND["PARAM_CEA"]
+    CONST_VF = DIC_COND["CONST_VF"]
+# %%
+    # MOX_RANGE = np.arange(10.0, 30.0, 1.0)*1e-3
+    # DT_RANGE = np.arange(4.0, 6.0, 0.5)*1e-3
+    # inst = Gen_excond_table(PARAM_EX, PARAM_LIQUID, PARAM_CEA, CONST_VF, mox_range=MOX_RANGE, Dt_range=DT_RANGE)
+    inst = Gen_excond_table(PARAM_EX, PARAM_LIQUID, PARAM_CEA, CONST_VF)
 
 # %%
-    # inst = Cal_excond(CALCOND, CONST_VF)
-    # out = inst.get_excond(mox=15.0e-3, Dt=7.0e-3, Pc_init=0.3e+6)
-    # print(out)
-
-    MOX_RANGE = np.arange(10.0, 30.0, 1.0)*1e-3
-    DT_RANGE = np.arange(4.0, 6.0, 0.5)*1e-3
-    inst = Gen_excond_table(PARAM_EX, PARAM_LIQUID, PARAM_CEA, CONST_VF, mox_range=MOX_RANGE, Dt_range=DT_RANGE)
-
-# %%
-    # inst = Gen_excond_table(CALCOND, CONST_VF)
-
+## Output the calculated result as a csv data
     out = inst.gen_table()
-    inst.output()
+    inst.output(fldname=FLDNAME)
     print("Suceeded!")
-
-
 #%%
---
+++ sample/cond.py
@@ -0,0 +1,56 @@
+# Condition file for main_design.py
+import os
+
+PARAM_EX = {"d": 0.3e-3,     # [m] port diameter
+            "N": 432,        # [-] the number of port
+            "Df": 38.0e-3,   # [m] fuel outer diameter
+            "eta": 0.83,     # [-] efficiency of characteristic exhaust velocity
+            "rho_f": 1191,   # [kg/m3] density of fuel
+            "Rm": 259.8,     # [J/kg/K] gas constant
+            "Tox": 300,      # [K] oxidizer tempereture
+            "mu": 20.3e-6    # [Pa-s] oxidizer dynamic viscosity
+            }
+
+PARAM_LIQUID = {"mode_liquid": False, # mode selection; using liquid oxidizer or using gasous oxidizer.
+                "C": 0.61,       # discharge coefficient of orifice
+                "be": 0.25,      # diamter ratio between pipe and orifice diameter
+                "d_o": 1.7e-3,   # [m] diameter of orifice
+                "rho_ox": 1190   # [kg/m3] density of liquid oxidizer
+                }
+
+PARAM_CEA = {"cea_path": os.path.join("cea_db", "GOX_CurableResin", "csv_database"),   # relative folder path to CEA .csv data-base
+             "mode_n2": False,               # mode selection; using Gasous N2 for pressurization or not.
+                                             # If True, this code execute CEA as a single execute mode using following data,
+                                             # If False, this code execute CEA using the .csv data base assigned at the above key: "cea_db".
+             "massfrac_n2": 0.1,             # mass fraction of Gaseous N2 to fuel and oxidizer mass flow rate
+             "eq_option": "frozen nfz=2",    # (CEA input) equilibrium option for NASA-CEA
+             "eps": 1.0,                     # (CEA input) nozzle opening ratio
+             "name_oxid": "O2(L)",           # (CEA input) the name of oxidizer
+             "temp_oxid": 90,                # (CEA input) [K] the temperature of oxidizer
+             "name_fuel": "CurableResin",    # (CEA input) the name of fuel
+             "temp_fuel": 90,                # (CEA input) [K] the temperature of fuel
+             "enthalpy": -296.9636,          # (CEA input) [kJ/mol]
+             "element": "C 16.0873 H 20.6143 O 3.96810"     # (CEA input) the kind of element and its moleculer number
+            }
+
+CONST_VF = {"mode": "C1C2",     # mode selectioin.
+                                # "C1C2" mode uses the following experimental formula: Vf = (C1/Vox+C2)P^n, 
+                                # "EXP" mode uses the following experimental formula: Vf = beta*P^n*Vox^m,
+                                # "PROP" mode uses the following experimental formula: Vf = alpha*P^n
+                                # "LIQUID" mode for liquid oxidizer uses the following experimental fromaula:
+                                #  Vf = (C1(d)/Vox+C2(d))P^n, C1(d) = (C11*d^m1 + C12), C2(d) = (C21*d^m2 + C22)
+            "n": 1.0,           # pressure exponent
+            "C1": 9.34e-8,      # SI-unit, optional. experimental constant
+            # "C1": 13.9e-8,      # SI-unit, optional. experimental constant
+            "C2": 2.46e-9,      # SI-unit, optional. experimental constant
+            # "C2": 1.61e-9,      # SI-unit, optional. experimental constant
+            "beta": None,       # SI-unit, optional. experimental constant
+            "m": None,          # [-] optional. exponent of oxidizer port velocity
+            "alpha": None,      # SI-unit, optional. experimental constant
+            "C11": -104e-9,     # SI-unit, optional. experimental constant
+            "C12": 3.55e-9,     # SI-unit, optional. experimental constant
+            "m1": 0.715,        # [-] optional. exponent of port diameter
+            "C21": 43.4e-9,     # SI-unit, optional. experimental constant
+            "C22": -0.905e-9,   # SI-unit, optional. experimental constant
+            "m2": 0.426         # [-] optional. exponent of port diameter
+            }
